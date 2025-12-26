9a03234d68099d36b43c3864df67a486f0b41186
Javed <jawed_tahir@yahoo.fr>
2025-12-26 17:50:37 +0100

    Fix: Update drizzle.config.ts to new syntax

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from 'drizzle-kit'
+import type { Config } from 'drizzle-kit';
 
 export default {
-  schema: './src/db/schema.ts',
+  schema: './lib/db/schema.ts',
   out: './drizzle',
-  driver: 'pg',
+  dialect: 'postgresql',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!
-  }
-} satisfies Config
+    url: process.env.DATABASE_URL!,
+  },
+} satisfies Config;
